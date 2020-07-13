232d29d777609d4ddefd373f1db867bcbd970441
K-Bellefeuille <67068743+K-Bellefeuille@users.noreply.github.com>
2020-07-12 20:56:16 -0600

    test push

--- craigslist-pro/server/index.js
+++ craigslist-pro/server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const { request, response } = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 const app = express();
 
@@ -9,11 +10,53 @@ app.use(cors());
 
 app.use(morgan('tiny'));
 
-app.get('/',( request, response) => {
+function getResults(body) {
+  const $ = cheerio.load(body);
+  const rows = $('li.result-row');
+  const results = [];
+
+  rows.each((index, element) => {
+    const result = $(element);
+    const title = result.find('.result-title').text();
+    const price = $(result.find('.result-price').get(0)).text();
+    const image = result.find('div.swipe-wrap div[data-index="0"] img').attr('src');
+    results.push({
+      title,
+      price,
+      image
+    });
+  });
+  return results;
+
+}
+
+app.get('/', (request, response) => {
   response.json({
     message: "Hello World 👋"
   });
 });
+
+app.get('/search/:location/:search_term', (request, response) => {
+  const {
+    location,
+    search_term
+  } = request.params;
+  const url = `https://${location}.craigslist.org/search/msa?sort=date&query=${search_term}`;
+  fetch(url)
+    .then(response => response.text())
+    .then(body => {
+      const results = getResults(body);
+      response.json({
+        results
+      });
+    });
+
+  //response.json({
+  //   result: [],
+  //   url
+  // });
+});
+
 
 app.use('/', (request, response, next) => {
   const error = new Error("not found");
@@ -21,7 +64,7 @@ app.use('/', (request, response, next) => {
   next(error);
 });
 
-app.use((error, request, response, next)=>{
+app.use((error, request, response, next) => {
   response.status(response.statsCode || 500);
   response.json({
     message: error.message
@@ -29,7 +72,6 @@ app.use((error, request, response, next)=>{
 
 });
 
-app.listen(5000,() => {
+app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
-
